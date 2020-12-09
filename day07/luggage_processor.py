4a7527d17feb916cd8a9f2a41f3842c9f9db2031
countless-integers <oxygen-nitrogen@outlook.com>
2020-12-09 21:22:20 +0100

    day07: well this hurt a little

--- day07/luggage_processor.py
+++ day07/luggage_processor.py
@@ -3,11 +3,11 @@ from re import findall
 
 
 def parse_row(row : str) -> dict:
-    matches = findall(r"([a-z ]*?)bags?", row)
-    matches = [match.strip() for match in matches]
-    if matches[1] == "contain no other":
-        return {matches[0]: []}
-    return {matches[0]: matches[1:]}
+    matches = findall(r"(\d+ )?([a-z ]*?)bags?", row)
+    # matches = [match.strip() for match in matches]
+    if matches[1][1].strip() == "contain no other":
+        return {matches[0][1].strip(): {}}
+    return {matches[0][1].strip(): {k.strip(): int(v) for v, k in matches[1:]}}
 
 
 def find_bags_containing(target : str, bags : dict) -> set:
@@ -22,6 +22,16 @@ def find_bags_containing(target : str, bags : dict) -> set:
     return seen_in
 
 
+def count_bags_in_bags(target : str, bags : dict) -> int:
+    def count_bags(target : str) -> int:
+        count = 1
+        for bag in bags[target]:
+            count += bags[target][bag] * count_bags(bag)
+        return count
+
+    return count_bags(target) - 1
+
+
 if __name__ == "__main__":
     dir_path = dirname(realpath(__file__))
 
@@ -33,3 +43,4 @@ if __name__ == "__main__":
             bags.update(parse_row(row))
 
     print(len(find_bags_containing('shiny gold', bags)))
+    print(count_bags_in_bags('shiny gold', bags))
--- day07/test_luggage_processor.py
+++ day07/test_luggage_processor.py
@@ -1,5 +1,5 @@
 import pytest
-from luggage_processor import find_bags_containing, parse_row
+from luggage_processor import find_bags_containing, parse_row, count_bags_in_bags
 
 
 @pytest.mark.parametrize(
@@ -7,15 +7,15 @@ from luggage_processor import find_bags_containing, parse_row
     [
         pytest.param(
             {
-                "light red": ["bright white", "muted yellow"],
-                "dark orange": ["bright white", "muted yellow"],
-                "bright white": ["shiny gold"],
-                "muted yellow": ["shiny gold", "faded blue"],
-                "shiny gold": ["dark olive", "vibrant plum"],
-                "dark olive": ["faded blue", "dotted black"],
-                "vibrant plum": ["faded blue", "dotted black"],
-                "faded blue": [],
-                "dotted black": [],
+                "light red": {"bright white": 1, "muted yellow": 2},
+                "dark orange": {"bright white": 3, "muted yellow": 4},
+                "bright white": {"shiny gold": 1},
+                "muted yellow": {"shiny gold": 2, "faded blue": 9},
+                "shiny gold": {"dark olive": 1, "vibrant plum": 2},
+                "dark olive": {"faded blue": 3, "dotted black": 4},
+                "vibrant plum": {"faded blue": 5, "dotted black": 6},
+                "faded blue": {},
+                "dotted black": {},
             },
             4,
             id="example 1",
@@ -24,3 +24,48 @@ from luggage_processor import find_bags_containing, parse_row
 )
 def test_find_bags_containing(bags : dict, expected_count : int) -> None:
     assert expected_count == len(find_bags_containing("shiny gold", bags))
+
+
+@pytest.mark.parametrize(
+    "row,expected_result",
+    [
+        pytest.param(
+            "dotted black bags contain no other bags.",
+            {"dotted black": {}}
+        ),
+        pytest.param(
+            "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+            {"vibrant plum": {
+                "faded blue": 5,
+                "dotted black": 6,
+            }}
+        )
+
+    ]
+)
+def test_parse_row(row : str, expected_result : dict) -> None:
+    assert expected_result == parse_row(row)
+
+
+@pytest.mark.parametrize(
+    "bags,expected_count",
+    [
+        pytest.param(
+            {
+                "light red": {"bright white": 1, "muted yellow": 2},
+                "dark orange": {"bright white": 3, "muted yellow": 4},
+                "bright white": {"shiny gold": 1},
+                "muted yellow": {"shiny gold": 2, "faded blue": 9},
+                "shiny gold": {"dark olive": 1, "vibrant plum": 2},
+                "dark olive": {"faded blue": 3, "dotted black": 4},
+                "vibrant plum": {"faded blue": 5, "dotted black": 6},
+                "faded blue": {},
+                "dotted black": {},
+            },
+            32,
+            id="example 1"
+        )
+    ]
+)
+def test_count_bags_in_bags(bags : dict, expected_count : int) -> None:
+    assert expected_count == count_bags_in_bags('shiny gold', bags)
