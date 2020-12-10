d4f2000dd596ab104b35f0a30a077c6b690f5c0e
countless-integers <oxygen-nitrogen@outlook.com>
2020-12-10 20:08:08 +0100

    day08: part 2, the deep uno reverse switch plot twist

--- day08/boot_sequence.py
+++ day08/boot_sequence.py
@@ -1,14 +1,22 @@
 from os.path import dirname, realpath
+from copy import deepcopy 
 
 
-def boot(instructions : list) -> int:
+def boot(instructions : list, use_exceptions = False) -> int:
     index, accumulator = 0, 0
     executed_instructions = {}
+    instructions_length = len(instructions)
 
     while True:
+        if index + 1 >  instructions_length:
+            break 
+
         instruction, value = instructions[index]
         if index in executed_instructions:
+            if use_exceptions:
+                raise RecursionError("Infinite loop detected")
             break
+
         executed_instructions[index] = 1
         if instruction == "nop":
             index += 1
@@ -24,7 +32,24 @@ def boot(instructions : list) -> int:
     return accumulator
 
 
+def uno_boot(instructions : list) -> int:
+    for index, instruction in enumerate(instructions):
+        if instruction[0] == "jmp" or instruction[0] == "nop":
+            # note to self:
+            # because it's a list of lists shallow copy won't work, so we
+            # need to go deep...
+            new_instructions = deepcopy(instructions)
+            new_instructions[index][0] = "jmpnop".replace(instruction[0], "")
+            try:
+                return boot(new_instructions, use_exceptions=True)
+            except RecursionError:
+                continue
+    raise Exception("the ultimate failure")
+
+
 if __name__ == "__main__":
     dir_path = dirname(realpath(__file__))
     with open(dir_path + "/input.txt") as input_file:
-        print(boot([row.strip().split() for row in input_file]))
+        instructions = [row.strip().split() for row in input_file]
+        print(boot(instructions))
+        print(uno_boot(instructions))
--- day08/test_boot_sequence.py
+++ day08/test_boot_sequence.py
@@ -1,5 +1,5 @@
 import pytest
-from boot_sequence import boot
+from boot_sequence import boot, uno_boot
 
 
 @pytest.mark.parametrize(
@@ -22,5 +22,29 @@ from boot_sequence import boot
         ),
     ]
 )
-def test_find_bags_containing(instructions : list, expected_acc : int) -> None:
+def test_boot(instructions : list, expected_acc : int) -> None:
     assert expected_acc == boot(instructions)
+
+
+@pytest.mark.parametrize(
+    "instructions,expected_acc",
+    [
+        pytest.param(
+            [
+                ["nop", "+0"],
+                ["acc", "+1"],
+                ["jmp", "+4"],
+                ["acc", "+3"],
+                ["jmp", "-3"],
+                ["acc", "-99"],
+                ["acc", "+1"],
+                ["jmp", "-4"],
+                ["acc", "+6"],
+            ],
+            8,
+            id="replace 8th jmp with nop",
+        ),
+    ]
+)
+def test_boot_uno(instructions : list, expected_acc : int) -> None:
+    assert expected_acc == uno_boot(instructions)
