1b9c0f16cd88ef7a865d2be8cbbace31449a45b6
Adrian <totcha@gazeta.pl>
2020-12-16 19:54:55 +0100

    day12: part 2

--- day12/rain_risk.py
+++ day12/rain_risk.py
@@ -4,8 +4,8 @@ from typing import List, Tuple
 
 def move(from_position : dict, instruction : str) -> dict:
     position = {
-        "x": from_position["x"], 
-        "y": from_position["y"], 
+        "x": from_position["x"],
+        "y": from_position["y"],
         "dir": from_position["dir"]
     }
     action = instruction[0]
@@ -45,8 +45,58 @@ def calculate_distance(instructions : List[str]) -> int:
     return abs(current_position["x"]) + abs(current_position["y"])
 
 
+def move_to_waypoint(from_position : dict, instruction : str) -> dict:
+    position = {
+        "x": from_position["x"],
+        "y": from_position["y"],
+        "waypoint": {
+            "x": from_position["waypoint"]["x"],
+            "y": from_position["waypoint"]["y"],
+        }
+    }
+    action = instruction[0]
+    value = int(instruction[1:])
+    if action == "N":
+        position["waypoint"]["y"] += value
+    elif action == "S":
+        position["waypoint"]["y"] -= value
+    elif action == "E":
+        position["waypoint"]["x"] += value
+    elif action == "W":
+        position["waypoint"]["x"] -= value
+    elif action == "F":
+        position["y"] += position["waypoint"]["y"] * value
+        position["x"] += position["waypoint"]["x"] * value
+    elif action == "L" and value % 360 == 90 or action == "R" and value % 360 == 270:
+        position["waypoint"]["y"] = from_position["waypoint"]["x"]
+        position["waypoint"]["x"] = -from_position["waypoint"]["y"]
+    elif action == "L" and value % 360 == 270 or action == "R" and value % 360 == 90:
+        position["waypoint"]["y"] = -from_position["waypoint"]["x"]
+        position["waypoint"]["x"] = from_position["waypoint"]["y"]
+    elif action == "L" and value % 360 == 180 or action == "R" and value % 360 == 180:
+        position["waypoint"]["y"] = -from_position["waypoint"]["y"]
+        position["waypoint"]["x"] = -from_position["waypoint"]["x"]
+
+    return position
+
+
+def calculate_distance_with_waypoint(instructions : List[str]) -> int:
+    current_position = {
+        "x": 0,
+        "y": 0,
+        "waypoint": {
+            "x": 10,
+            "y": 1
+        }
+    }
+    for instruction in instructions:
+        current_position = move_to_waypoint(current_position, instruction)
+
+    return abs(current_position["x"]) + abs(current_position["y"])
+
 if __name__ == "__main__":
     dir_path = dirname(realpath(__file__))
     with open(dir_path + "/input.txt") as input_file:
         instructions = [row.strip() for row in input_file]
         print(calculate_distance(instructions))
+        print(calculate_distance_with_waypoint(instructions))
--- day12/test_rain_risk.py
+++ day12/test_rain_risk.py
@@ -1,5 +1,5 @@
 import pytest
-from rain_risk import calculate_distance, move
+from rain_risk import calculate_distance, move, move_to_waypoint, calculate_distance_with_waypoint
 from typing import List
 
 
@@ -21,3 +21,23 @@ from typing import List
 )
 def test_calculate_distance(instructions : List[str], expected_distance : int) -> None:
     assert expected_distance == calculate_distance(instructions)
+
+
+@pytest.mark.parametrize(
+    "instructions,expected_distance",
+    [
+        pytest.param(
+            [
+                "F10",
+                "N3",
+                "F7",
+                "R90",
+                "F11",
+            ],
+            286,
+            id="example 1",
+        ),
+    ]
+)
+def test_calculate_distance_with_waypoint(instructions : List[str], expected_distance : int) -> None:
+    assert expected_distance == calculate_distance_with_waypoint(instructions)
