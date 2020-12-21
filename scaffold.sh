5bd0d9aaced0ef285cb32a436c36eed3dc928da9
countless-integers <oxygen-nitrogen@outlook.com>
2020-12-21 14:35:07 +0100

    chore: add some boilerplate to scaffolder

--- scaffold.sh
+++ scaffold.sh
@@ -10,3 +10,36 @@ mkdir $dir
 touch $dir/$2.py
 touch $dir/test_$2.py
 touch $dir/input.txt
+
+cat > $dir/test_$2.py <<CONTENT
+import pytest
+from $2 import method
+
+
+@pytest.mark.parametrize(
+    ["value", "expected_value"],
+    [
+        pytest.param(
+            11,
+            73,
+            id="example 1",
+        ),
+    ]
+)
+def test_method(value : int, expected_value : int) -> None:
+    assert expected_value == method(mask, value)
+CONTENT
+
+cat > $dir/$2.py <<CONTENT
+from os.path import dirname, realpath
+
+
+def method():
+    pass
+
+
+if __name__ == "__main__":
+    dir_path = dirname(realpath(__file__))
+    with open(dir_path + "/input.txt") as input_file:
+        pass
+CONTENT
